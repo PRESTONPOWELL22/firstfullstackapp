20b40f276f4da3c16ffd342056ab3d89ff9e1b6e
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-04-09 16:00:34 -0700

    got a couple of things working

--- server.js
+++ server.js
@@ -6,13 +6,8 @@ var app = express()
 
 app.use(express.static(path.join(__dirname, 'app/public')))
 
-
-app.get('/this', function (req, res) {
-  res.send('<h1>you suck</h1>')
+app.get('/survey', function (req, res) {
+  res.sendFile(path.join(__dirname, 'app/public/survey.html'))
 })
 
-app.get('/that', function (req, res) {
-  res.send('hello')
-})
-
-app.listen(3010)
+app.listen(3011)
